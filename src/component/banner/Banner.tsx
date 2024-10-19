dbe344223d048be32040cf77f3202b309a355367
riste <riste.vasilevski@gmail.com>
2024-10-19 10:10:10 +0200

    banner update

--- src/component/banner/Banner.tsx
+++ src/component/banner/Banner.tsx
@@ -1,8 +1,11 @@
 import bannerStyle from "./banner.module.css";
 
+import img1 from "./image/bannerimg.png";
+
 export const Banner = () => {
   return (
     <div className={bannerStyle.banner}>
+      <img src={img1} alt="image" />
       <p>
         Bezbedna i brza isporaka na kupenite proizvodi do <br />
         <span>vasata adresa</span>
