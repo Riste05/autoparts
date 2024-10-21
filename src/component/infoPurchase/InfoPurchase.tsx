510db1e2ee620ce1ff70578401698dca9c0e4e78
riste <riste.vasilevski@gmail.com>
2024-10-21 22:14:41 +0200

    update component

--- src/component/infoPurchase/InfoPurchase.tsx
+++ src/component/infoPurchase/InfoPurchase.tsx
@@ -12,24 +12,30 @@ export const InfoPurchase = () => {
     <div className={purchaseStyle.container}>
       <div className={purchaseStyle.item}>
         <FontAwesomeIcon icon={faWarehouse} className={purchaseStyle.img} />
-        <h3>Голем спектар на прозиводи</h3>
-        <p>Сите видови на авто делови за сите типови на возила</p>
+        <div className={purchaseStyle.content}>
+          <h3>Голем спектар на прозиводи</h3>
+          <p>Сите видови на авто делови за сите типови на возила</p>
+        </div>
       </div>
       <div className={purchaseStyle.item}>
         <FontAwesomeIcon icon={faTruckFast} className={purchaseStyle.img} />
-        <h3>Брза достава 1-2 работни дена</h3>
-        <p>
-          Во зависнот во кој дел од денот се прави нарачката доставата е за 1
-          или 2 дена
-        </p>
+        <div className={purchaseStyle.content}>
+          <h3>Брза достава 1-2 работни дена</h3>
+          <p>
+            Во зависнот во кој дел од денот се прави нарачката доставата е за 1
+            или 2 дена
+          </p>
+        </div>
       </div>
       <div className={purchaseStyle.item}>
         <FontAwesomeIcon icon={faRotateLeft} className={purchaseStyle.img} />
-        <h3>Рок на враќање 14 дена</h3>
-        <p>
-          Доколку производот се врати во првобитната состајба (да не е монтиран
-          и во неоштетана опаковка) ќе ви бидат вратени парите
-        </p>
+        <div className={purchaseStyle.content}>
+          <h3>Рок на враќање 14 дена</h3>
+          <p>
+            Доколку производот се врати во првобитната состајба (да не е
+            монтиран и во неоштетана опаковка) ќе ви бидат вратени парите
+          </p>
+        </div>
       </div>
     </div>
   );
