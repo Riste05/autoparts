a7937f6cb87642dccdd88b7fe4b959a9d2a19cde
riste <riste.vasilevski@gmail.com>
2024-10-24 10:28:35 +0200

    update data

--- src/data/swiperData.tsx
+++ src/data/swiperData.tsx
@@ -6,26 +6,31 @@ import img5 from "../pages/home/component/header/image/img5.png";
 
 const swiperData = [
   {
+    id: "swipper1",
     img: img2,
     title: "Кочионен систем",
     text: "Диск плочки, Дискови, Гуртни, Kлипчиња, црево за кочници",
   },
   {
+    id: "swipper2",
     img: img1,
     title: "Менувач",
     text: "Сет кумплунг, Замаец, Друк лагер",
   },
   {
+    id: "swipper3",
     img: img4,
     title: "Каиш и Затегачи",
     text: "Ребраст, Клинаст, Канален, Затегачи",
   },
   {
+    id: "swipper4",
     img: img5,
     title: "Дихтунг",
     text: "Глава, Вентили, Усис, Издув, Гумици вентили, Гарнитури",
   },
   {
+    id: "swipper5",
     img: img3,
     title: "Филтри",
     text: "Воздух, Кабина, Масло, Гориво",
