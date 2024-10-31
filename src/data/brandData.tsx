a4d54fceaf3a46598bf05d623c53416eceb21b0f
riste <riste.vasilevski@gmail.com>
2024-10-31 13:54:23 +0100

    update brend component

---
+++ src/data/brandData.tsx
@@ -0,0 +1,65 @@
+import airtex from "../pages/home/component/brend/image/AIRTEX.jpeg";
+import aisin from "../pages/home/component/brend/image/AISIN.jpeg";
+import bosch from "../pages/home/component/brend/image/Bosch.png";
+import vernet from "../pages/home/component/brend/image/CALORSTAT by Vernet.jpeg";
+import fag from "../pages/home/component/brend/image/FAG.jpeg";
+import hitachi from "../pages/home/component/brend/image/HITACHI.jpeg";
+import ina from "../pages/home/component/brend/image/Ina.png";
+import mann from "../pages/home/component/brend/image/MANN-FILTER.jpeg";
+import monroe from "../pages/home/component/brend/image/MONROE.jpeg";
+import skf from "../pages/home/component/brend/image/SKF.jpeg";
+import trw from "../pages/home/component/brend/image/TRW.jpeg";
+import valeo from "../pages/home/component/brend/image/VALEO.jpeg";
+
+const brendData = [
+  {
+    img: airtex,
+    info: "Airtex",
+  },
+  {
+    img: aisin,
+    info: "Aisin",
+  },
+  {
+    img: bosch,
+    info: "Bosch",
+  },
+  {
+    img: vernet,
+    info: "Vernet",
+  },
+  {
+    img: fag,
+    info: "Fag",
+  },
+  {
+    img: hitachi,
+    info: "Hitachi",
+  },
+  {
+    img: ina,
+    info: "Ina",
+  },
+  {
+    img: mann,
+    info: "Mann-Filter",
+  },
+  {
+    img: monroe,
+    info: "Monroe",
+  },
+  {
+    img: skf,
+    info: "SKF",
+  },
+  {
+    img: trw,
+    info: "TRW",
+  },
+  {
+    img: valeo,
+    info: "Valeo",
+  },
+];
+
+export default brendData;
