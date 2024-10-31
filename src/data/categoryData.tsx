ba0cf7e408b0fe31f99e669b4a51e72c82d750bb
riste <riste.vasilevski@gmail.com>
2024-10-31 13:30:51 +0100

    update category component

---
+++ src/data/categoryData.tsx
@@ -0,0 +1,37 @@
+import sopiranje from "../pages/home/component/kategorii/image/sopiranje.jpg";
+import filtri from "../pages/home/component/kategorii/image/filtri.jpg";
+import kais from "../pages/home/component/kategorii/image/kais.jpg";
+import menuvac from "../pages/home/component/kategorii/image/menuvac.jpg";
+import amortizeri from "../pages/home/component/kategorii/image/amortizeri.jpg";
+import maslo from "../pages/home/component/kategorii/image/maslo.jpg";
+
+const categoryData = [
+  {
+    img: sopiranje,
+    info: "Кочионен систем",
+  },
+  {
+    img: filtri,
+    info: "Филтри",
+  },
+  {
+    img: kais,
+    info: "Каиш",
+  },
+  {
+    img: menuvac,
+    info: "Менувач",
+  },
+
+  {
+    img: amortizeri,
+    info: "Амортизери",
+  },
+
+  {
+    img: maslo,
+    info: "Масло",
+  },
+];
+
+export default categoryData;
--- src/pages/home/component/kategorii/CategoryParts.tsx
+++ src/pages/home/component/kategorii/CategoryParts.tsx
@@ -1,11 +1,5 @@
 import categoryStyle from "./CategoryParts.module.css";
-
-import amortizeri from "./image/amortizeri.jpg";
-import filtri from "./image/filtri.jpg";
-import kais from "./image/kais.jpg";
-import maslo from "./image/maslo.jpg";
-import menuvac from "./image/menuvac.jpg";
-import sopiranje from "./image/sopiranje.jpg";
+import categoryData from "../../../../data/categoryData";
 
 export const CategoryParts = () => {
   return (
@@ -14,31 +8,12 @@ export const CategoryParts = () => {
         <h1 className={categoryStyle.text}>Категории на авто делови</h1>
 
         <div className={categoryStyle.category}>
-          <div className={categoryStyle.item}>
-            <img src={sopiranje} alt="image" />
-            <p>Кочионен систем</p>
-          </div>
-          <div className={categoryStyle.item}>
-            <img src={filtri} alt="image" />
-            <p>Филтри</p>
-          </div>
-          <div className={categoryStyle.item}>
-            <img src={kais} alt="image" />
-            <p>Каиш</p>
-          </div>
-
-          <div className={categoryStyle.item}>
-            <img src={menuvac} alt="image" />
-            <p>Менувач</p>
-          </div>
-          <div className={categoryStyle.item}>
-            <img src={amortizeri} alt="image" />
-            <p>Амортизери</p>
-          </div>
-          <div className={categoryStyle.item}>
-            <img src={maslo} alt="image" />
-            <p>Масло</p>
-          </div>
+          {categoryData.map((item) => (
+            <div className={categoryStyle.item} key={item.info}>
+              <img src={item.img} alt="image" />
+              <p>{item.info}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
