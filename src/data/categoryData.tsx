57b7f02484086ce0c5b9a9b9ee26c109db68463a
riste <riste.vasilevski@gmail.com>
2024-11-16 14:03:35 +0100

    data update

--- src/data/categoryData.tsx
+++ src/data/categoryData.tsx
@@ -3,6 +3,13 @@ import sopiranje from "../pages/home/component/kategorii/image/sopiranje.png";
 import menuvac from "../pages/home/component/kategorii/image/menuvac.png";
 import kais from "../pages/home/component/kategorii/image/kais.png";
 import dihtung from "../pages/home/component/kategorii/image/dihtung.png";
+import amortizer from "../pages/home/component/kategorii/image/amortizer.png";
+import prednica from "../pages/home/component/kategorii/image/prednica.png";
+import zglob from "../pages/home/component/kategorii/image/zglob.png";
+import vodnaPumpa from "../pages/home/component/kategorii/image/pumpa-voda.png";
+import svekica from "../pages/home/component/kategorii/image/svekica.png";
+import sijalica from "../pages/home/component/kategorii/image/sijalica.png";
+import metlici from "../pages/home/component/kategorii/image/metlici.png";
 
 const categoryData = [
   {
@@ -24,6 +31,34 @@ const categoryData = [
   {
     img: dihtung,
     info: "Дихтунг",
+  },
+  {
+    img: amortizer,
+    info: "Амортизер",
+  },
+  {
+    img: prednica,
+    info: "Систем за управување",
+  },
+  {
+    img: zglob,
+    info: "Погон на тркала",
+  },
+  {
+    img: vodnaPumpa,
+    info: "Ладење",
+  },
+  {
+    img: svekica,
+    info: "Систем за палење",
+  },
+  {
+    img: sijalica,
+    info: "Сијалици",
+  },
+  {
+    img: metlici,
+    info: "Шофершајбна",
   },
 ];
 
