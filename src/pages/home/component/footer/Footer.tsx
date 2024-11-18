5f19cccd4bfe50e4e9b443988c7838c857d83f9d
riste <riste.vasilevski@gmail.com>
2024-11-18 12:06:43 +0100

    update data

--- src/pages/home/component/footer/Footer.tsx
+++ src/pages/home/component/footer/Footer.tsx
@@ -1,14 +1,10 @@
 import footerStyle from "./footer.module.css";
 
-import footerImage from "./image/footer_img.jpg";
-
 export const Footer = () => {
   return (
     <footer>
       <div className={footerStyle.container}>
-        <div className={footerStyle.image}>
-          <img src={footerImage} alt="" />
-        </div>
+        <h1>riste</h1>
       </div>
     </footer>
   );
--- src/pages/home/component/kategorii/CategoryParts.tsx
+++ src/pages/home/component/kategorii/CategoryParts.tsx
@@ -1,26 +1,32 @@
+import { useState, useEffect } from "react";
 import categoryStyle from "./CategoryParts.module.css";
 import categoryData from "../../../../data/categoryData";
 
-export const CategoryParts = ({ allCategory }) => {
+type CategoryPartsProps = {
+  allCategory: boolean;
+};
+
+export const CategoryParts = ({ allCategory }: CategoryPartsProps) => {
+  const [data, setData] = useState(categoryData);
+
+  useEffect(() => {
+    return !allCategory
+      ? setData((element) => element.slice(0, 6))
+      : setData(categoryData);
+  }, [allCategory]);
+
   return (
     <section className={categoryStyle.section}>
       <div className={categoryStyle.container}>
         <h1 className={categoryStyle.text}>Категории на авто делови</h1>
 
         <div className={categoryStyle.category}>
-          {!allCategory
-            ? categoryData.slice(0, 6).map((item) => (
-                <div className={categoryStyle.item} key={item.info}>
-                  <img src={item.img} alt="image" />
-                  <p>{item.info}</p>
-                </div>
-              ))
-            : categoryData.map((item) => (
-                <div className={categoryStyle.item} key={item.info}>
-                  <img src={item.img} alt="image" />
-                  <p>{item.info}</p>
-                </div>
-              ))}
+          {data.map((item) => (
+            <div className={categoryStyle.item} key={item.info}>
+              <img src={item.img} alt="image" />
+              <p>{item.info}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
